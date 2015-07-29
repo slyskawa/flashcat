8cedc5d8c29287c3f7ecf7ed7980a71e9e68bd3d
Steven Lyskawa <slyskawa@gmail.com>
2015-07-29 16:13:21 -0400

    Added counts, clear db tables, insert into sqlite db

--- listfile.py
+++ listfile.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
-#Listfile.py Version 3.1
+#Listfile.py Version 3.2
+
+#Command line arguments, drive, directory
 
 
 import os
@@ -14,17 +16,24 @@ def getVolumeID(dLetter):
     vID = os.popen('vol '+dLetter, 'r').read()  #Read windows volume information
     lvID = vID.splitlines()  #Break into two lines
     lvname = lvID[0].split("is") #Get volume name
+    print lvname
     vname = lvname[1].strip().replace(" ","_") #remove spaces from name
     vserial = lvID[1].split()[-1:][0]
     return vname,vserial
 
-    if len(sys.argv) > 1:
-        driveLetter=sys.argv[1]
+if len(sys.argv) == 2:
+    driveLetter=sys.argv[1]
+    directory=":\\"
+if len(sys.argv) == 3:
+    driveLetter=sys.argv[1]
+    directory=":\\"+sys.argv[2]
 
-x=driveLetter+":\\"
+x=driveLetter+directory
 volumeName,volumeSerial = getVolumeID(driveLetter)
 diskID="%s_%s"%(volumeName,volumeSerial)
 id = diskID
+dirCount = 0
+fileCount = 0
 
 dbName=id+".db3"
 db=sqlite3.connect(dbName)
@@ -33,18 +42,23 @@ print dbName
 fileDb=db.cursor()
 fileDb.execute("create table if not exists files(filename varchar(255),ext varchar,filesize integer,filedate varchar(30),path varchar(255),MediaID varchar,rundate varchar(30),hash varchar(255))")
 fileDb.execute("create table if not exists directory(dir varchar,id varchar)")
-#fileDb.execute("delete from files")
-#fileDb.execute("delete from directory")
+fileDb.execute("delete from files")
+fileDb.execute("delete from directory")
 filesBySize = {}
 def get_fingerprint(filename):
 
     try:
         file = open(filename,'rb')
+        loopCnt=20
         digest = hashlib.sha1()
         data = file.read(65536)
         while len(data) != 0:
             digest.update(data)
             data = file.read(65536)
+            #2012-01-23 Added loopcount for large files.
+            loopCnt = loopCnt - 1
+            if loopCnt == 0:
+                break
         file.close()
     except:
         return '0'
@@ -63,9 +77,12 @@ def get_fingerprint(filename):
 def walker(arg, dirname, fnames):
     d = os.getcwd()
     os.chdir(dirname)
+    fileCount = 0
     for f in fnames:
         if not os.path.isfile(f):
             fileDb.execute('''insert into directory(dir,id) values("%s","%s")'''%(f,id))
+            ldirCount.append(f)
+            print "\nDir %s"%(f)
         else:
             f=f.lower()
             size = os.stat(f)[stat.ST_SIZE]
@@ -73,19 +90,31 @@ def walker(arg, dirname, fnames):
             ext = os.path.splitext(f)
             extension= ext[1]
             fhash  = get_fingerprint(f)
+            dname=os.path.splitdrive(dirname)[1]
             MediaID=id
-            todaysDate=datetime.datetime.now().strftime("%m/%d/%Y")
+            todaysDate=datetime.datetime.now().strftime("%Y-%m-%d")
             modifyDate = datetime.datetime.fromtimestamp(ftime)
-            tDbValues = (f,extension,size,modifyDate,dirname,MediaID,todaysDate,fhash)
+            tDbValues = (f,extension,size,modifyDate,dname,MediaID,todaysDate,fhash)
             fileDb.execute('''insert into files(filename,ext,filesize,filedate,path,MediaID,runDate,hash) values("%s","%s","%s","%s","%s","%s","%s","%s")'''%tDbValues)
-            print ".",
+            #print ".",
+            fileCount += 1
+            print "%i "%(fileCount),
             #print f,size,modifyDate,dirname,fhash
+    totalFiles.append(fileCount)
     os.chdir(d)
 
 
 print 'Scanning directory "%s"....' % x
+ldirCount = []
+totalFiles=[]
 os.path.walk(x, walker, filesBySize)
+dirCount=len(ldirCount)
+fileCount=sum(totalFiles)
+#added 2012-01-23
+#Dir field in directory table only contains dir.  Put in full path pulled.
+fileDb.execute("insert into directory SELECT distinct path, MediaID from files")
 db.commit()
 fileDb.close()
 db.close()
-
+print ldirCount
+print "\n\n%s\nDirectories=%i\nFiles=%i\nScanning complete!"%(dbName,dirCount,fileCount)
